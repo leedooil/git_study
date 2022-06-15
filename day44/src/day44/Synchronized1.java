69a3ef6ef6fa8877e1e68ebd93f3262bf46e0a25
leedooil <ldi316@naver.com>
2022-06-15 13:17:31 +0900

    day43

---
+++ day44/src/day44/Synchronized1.java
@@ -0,0 +1,42 @@
+package day44;
+
+public class Synchronized1 {
+
+	public static void main(String[] args) {
+		MyThreadB s1 = new MyThreadB();
+		Thread t1 = new Thread(s1, "A");
+		Thread t2 = new Thread(s1, "B");
+		Thread t3 = new Thread(s1, "C");
+
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+
+}
+
+class Ticketing {
+	int ticketNumber = 1;
+
+	public void ticketing() {
+		synchronized (this) {
+			if (ticketNumber > 0) {
+				System.out.println(Thread.currentThread().getName() + "가 티켓팅 성공");
+				ticketNumber--;
+			} else {
+				System.out.println(Thread.currentThread().getName() + "가 티켓팅 실패");
+			}
+			System.out.println(Thread.currentThread().getName() + "가 티켓팅 시도 후 티켓수:" + ticketNumber);
+		}
+	}
+}
+
+class MyThreadB implements Runnable {
+	Ticketing ticket = new Ticketing();
+
+	@Override
+	public void run() {
+		ticket.ticketing();
+	}
+
+}
