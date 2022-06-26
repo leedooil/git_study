902913ade36b8d25ee3dd75f73d1da1c43bf71f8
leedooil <ldi316@naver.com>
2022-06-26 20:51:38 +0900

    nintendoswitch

--- nintendo/src/nintendo/NintendoSwitch.java
+++ nintendo/src/nintendo/NintendoSwitch.java
@@ -2,61 +2,105 @@ package nintendo;
 
 public class NintendoSwitch {
 
-    private boolean power = false;
-    private AnimalCross game1;
-    private Zelda game2;
-    private SuperMario game3;
+	private boolean power = false;
+	private AnimalCross game1;
+	private Zelda game2;
+	private SuperMario game3;
 
-    public void powerOn() {
-        if (power) {
-            System.out.println("�씠誘� �쟾�썝�씠 耳쒖졇�엳�뒿�땲�떎.");
-            return;
+	public void powerOn() {
+		if (power) {
+			System.out.println("이미 전원이 켜져있습니다.");
+			return;
 
-        }
-        power = true;
-        System.out.println("�땶�뀗�룄 �뒪�쐞移� �쟾�썝�쓣 耳곗뒿�땲�떎.");
+		}
+		power = true;
+		System.out.println("닌텐도 스위치 전원을 켰습니다.");
 
-    }
+	}
 
-    public void powerOff() {
-        if (!power) {
-            System.out.println("�씠誘� �쟾�썝�씠 爰쇱졇�엳�뒿�땲�떎.");
-            return;
-        }
-        power = false;
-        System.out.println("�땶�뀗�룄 �뒪�쐞移섏쓽 �쟾�썝�쓣 猿먯뒿�땲�떎.");
-    }
+	public void powerOff() {
+		if (!power) {
+			System.out.println("이미 전원이 꺼져있습니다.");
+			return;
+		}
+		power = false;
+		System.out.println("닌텐도 스위치의 전원을 껐습니다.");
+	}
 
-    public void checkChip() {
-        if (!power) {
-            System.out.println("�땶�뀗�룄 �쟾�썝�쓣 癒쇱� 耳쒖＜�떆湲� 諛붾엻�땲�떎.");
-            return;
-        }
-        if (game1 != null) {
-            System.out.println("�쁽�옱 �옣李⑸릺�뼱 �엳�뒗 移⑹�" + game1.getName() + "�엯�땲�떎.");
+	public void checkChip() {
+		if (!power) {
+			System.out.println("닌텐도의 전원을 먼저 켜주시기 바랍니다.");
+			return;
+		}
+		if (game1 != null) {
+			System.out.println("현재 장착되어 있는 칩은" + game1.getName() + "입니다.");
 
-        } else if (game2 != null) {
-            System.out.println("�쁽�옱 �옣李⑸릺�뼱 �엳�뒗 移⑹�" + game2.getName() + "�엯�땲�떎.");
-        } else if (game3 != null) {
-            System.out.println("�쁽�옱 �옣李⑸릺�뼱 �엳�뒗 移⑹�" + game3.getName() + "�엯�땲�떎.");
-        } else {
-            System.out.println("�쁽�옱 移⑹씠 �옣李⑸릺�뼱 �엳吏� �븡�뒿�땲�떎.");
-        }
-        public void removeChip(){
-            game1=null;
-            game2=null;
-            game3=null;
-            System.out.println("寃뚯엫湲곗뿉 �옣李⑸릺�뼱�엳�뜕 移⑹쓣 �젣嫄� �뻽�뒿�땲�떎.");
-        }
-        public void setChip(AnimalCross animalcross){
-            game1 = animalcross;
-            game2 = null;
-            game3 = null;
-        }
-        public void setChip(){}
-        public void setChip(){}
+		} else if (game2 != null) {
+			System.out.println("현재 장착되어 있는 칩은" + game2.getName() + "입니다.");
+		} else if (game3 != null) {
+			System.out.println("현재 장착되어 있는 칩은" + game3.getName() + "입니다.");
+		} else {
+			System.out.println("현재 칩이 장착되어 있지 않습니다.");
+		}
+	}
 
+	public void removeChip() {
+		game1 = null;
+		game2 = null;
+		game3 = null;
+		System.out.println("게임기에 장착되어있던 칩을 제거 했습니다.");
+	}
 
-    }
+	public void setChip(AnimalCross animalcross) {
+		game1 = animalcross;
+		game2 = null;
+		game3 = null;
+		System.out.println(animalcross.getName() + "을/를 장착했습니다.");
+	}
+
+	public void setChip(Zelda zelda) {
+		game1 = null;
+		game2 = zelda;
+		game3 = null;
+		System.out.println(zelda.getName() + "을/를 장착했습니다.");
+	}
+
+	public void setChip(SuperMario superMario) {
+		game1 = null;
+		game2 = null;
+		game3 = superMario;
+		System.out.println(superMario.getName() + "을/를 장착했습니다.");
+	}
+
+	public void palyGame() {
+		if(!power) {
+			System.out.println("닌텐도의 전원을 먼저 켜주시기 바랍니다.");
+			return;
+		}
+		if(game1 != null) {
+			game1.launch();
+		}else if(game2 != null) {
+			game2.launch();
+		}else if(game3 !=null) {
+			game3.launch();
+		}else {
+			System.out.println("현재 칩이 장착되어있지 않습니다.");
+		}
+	}
+	public void exitGame() {
+		if(!power) {
+			System.out.println("닌텐도 전원을 먼저 켜주시기 바랍니다.");
+			return;
+		}
+		if(game1 != null) {
+			game1.exit();
+		}else if(game2 != null) {
+			game2.exit();
+		}else if(game3 !=null) {
+			game3.exit();
+		}else {
+			System.out.println("현재 칩이 장착되어있지 않습니다.");
+		}
+	}
 
 }
--- nintendo/src/nintendo/Play.java
+++ nintendo/src/nintendo/Play.java
@@ -1,4 +1,22 @@
 package nintendo;
 
 public class Play {
+	public static void main(String[] args) {
+		NintendoSwitch nintendoSwitch = new NintendoSwitch();
+		
+		AnimalCross aniCross = new AnimalCross();
+		Zelda zelda = new Zelda();
+		SuperMario superMario = new SuperMario();
+		
+		nintendoSwitch.palyGame();
+		
+		nintendoSwitch.powerOn();
+		nintendoSwitch.checkChip();
+		
+		nintendoSwitch.setChip(aniCross);
+		nintendoSwitch.checkChip();
+		
+		nintendoSwitch.palyGame();
+		nintendoSwitch.exitGame();
+	}
 }
